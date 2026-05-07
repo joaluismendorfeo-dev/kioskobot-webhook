d899064c5150aeceb332d91f66bbf38d8ca2b23f
Joaquín Méndez <joaluismendorfeo@gmail.com>
2026-05-07 22:30:01 +0000

    Auto-commit agent changes

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const VERIFY_TOKEN = 'kioskobot2026';
+
+const server = http.createServer((req, res) => {
+  try {
+    const url = new URL(req.url, 'http://localhost');
+    if (req.method === 'GET') {
+      const mode = url.searchParams.get('hub.mode');
+      const token = url.searchParams.get('hub.verify_token');
+      const challenge = url.searchParams.get('hub.challenge');
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        res.writeHead(200);
+        res.end(challenge);
+      } else {
+        res.writeHead(403);
+        res.end('Forbidden');
+      }
+    } else {
+      res.writeHead(200);
+      res.end('OK');
+    }
+  } catch (e) {
+    res.writeHead(500);
+    res.end('Error');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('Servidor corriendo en puerto ' + PORT);
+});
